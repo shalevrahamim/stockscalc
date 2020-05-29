59fff4733213088cdf59f0bc333ead667a8713b8
Shalev Rahamim <srahamim@corp.devalore.com>
2020-05-29 17:40:43 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+const mkdirp = require('mkdirp')
 const fs = require('fs');
 const snp500 = require('./company list/s&p500');
 const DataCollector = require('./dataCollector');
 
+const STORE_PATH = './stocks data/'
+
 const saveData = (path, data) => {
     return new Promise((res, rej) => {
-        path = `./stock data/${path}`
-        fs.writeFile(path , data, (err) => {
+        path = STORE_PATH + path;
+        fs.writeFile(path , JSON.stringify(data), (err) => {
             if(err)
                 rej(err)
             res(`${path} updated successfully`);
@@ -13,6 +16,11 @@ const saveData = (path, data) => {
     });
 }
 
+(async () => {
+    let folders = ['daily', 'weekly', 'mounthly', 'SMA'];
+    for (const folder of folders)
+        await mkdirp(STORE_PATH + folder);
+})();
 let symbols = snp500.map(company => company.Symbol);
 let collector = new DataCollector(symbols);
 collector.on('onGetSMA', (symbol, data) => saveData(`SMA/${symbol}.json`, data).then((msg) => console.log(msg)));
